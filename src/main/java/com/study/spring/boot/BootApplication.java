986c71904ab870ceafd848ceefde50bc3ce19a74
longtao <javalongtao@163.com>
2023-02-08 15:55:29 +0800

    开启异步

--- src/main/java/com/study/spring/boot/BootApplication.java
+++ src/main/java/com/study/spring/boot/BootApplication.java
@@ -2,8 +2,9 @@ package com.study.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-
+@EnableAsync
 @SpringBootApplication
 public class BootApplication {
 
