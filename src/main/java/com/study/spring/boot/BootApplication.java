7b8aea147c009328eeef6853c2554b2e0bcb5cf7
longtao <javalongtao@163.com>
2023-02-08 15:54:13 +0800

    代码清理

--- src/main/java/com/study/spring/boot/BootApplication.java
+++ src/main/java/com/study/spring/boot/BootApplication.java
@@ -1,13 +1,9 @@
 package com.study.spring.boot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
 @SpringBootApplication
-@MapperScan("com.study.spring.boot.mapper")
 public class BootApplication {
 
 
